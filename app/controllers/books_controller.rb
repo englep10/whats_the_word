c9d3ae7f23a97df6d1c57d91450bcb78c52e7940
Philip Engles <englep10@gmail.com>
2015-06-15 22:23:52 -0400

    added crud to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,52 @@
 class BooksController < ApplicationController
+
+	before_action :find_book, only:[:show, :edit, :update, :destroy]
+
+	def index 
+		@books = Book.all
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to root_path
+		else 
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @book.update(book_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@book.destroy
+		redirect_to root_path
+	end
+
+
+	private
+		def book_params
+			params.require(:book).permit(:title, :author, :description)
+		end
+
+		def find_book
+			@book = Book.find(params[:id])
+		end
+
 end
